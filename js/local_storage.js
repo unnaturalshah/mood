1e22bfa591bbccbc0a69338f81cdacad79239cbd
Unnat Shah <unnatshah@gmail.com>
2023-05-28 04:38:11 -0400

    Update local_storage.js
    Signed-off-by: Unnat Shah <unnatshah@gmail.com>

--- js/local_storage.js
+++ js/local_storage.js
@@ -1,1 +1,19 @@
+// Store data in local storage
+function setLocalStorage(key, value) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error('Error setting data in local storage:', error);
+  }
+}
 
+// Retrieve data from local storage
+function getLocalStorage(key) {
+  try {
+    const data = localStorage.getItem(key);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error retrieving data from local storage:', error);
+    return null;
+  }
+}
