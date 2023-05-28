3f92806fd615cc54fd5cd4e504fbcfe19c4a6cad
Unnat Shah <unnatshah@gmail.com>
2023-05-28 05:21:54 -0400

    Update reinforcement_agent.js
    Signed-off-by: Unnat Shah <unnatshah@gmail.com>

--- js/reinforcement_agent.js
+++ js/reinforcement_agent.js
@@ -3,13 +3,13 @@ function generateAgentRecommendation(mood) {
 
   switch (mood) {
     case 'happy':
-      recommendation = ';] Great to see you happy! Keep up the positive energy!';
+      recommendation = 'Great to see you happy! Keep up the positive energy!';
       break;
     case 'neutral':
-      recommendation = ':| Feeling neutral? Take some time for self-reflection and self-care.';
+      recommendation = 'Feeling neutral? Take some time for self-reflection and self-care.';
       break;
     case 'unhappy':
-      recommendation = ';[ If you are feeling unhappy, remember to reach out for support. You are not alone.';
+      recommendation = 'If you are feeling unhappy, remember to reach out for support. You are not alone.';
       break;
     default:
       recommendation = 'No recommendation available for the selected mood.';
@@ -33,56 +33,4 @@ function captureUserData(mood, color) {
     mood: mood,
     color: color,
     timestamp: new Date().toISOString()
-  };
-
-  const userHistory = getLocalStorage('userHistory') || [];
-  userHistory.push(userData);
-  setLocalStorage('userHistory', userHistory);
-}
-
-function openFeedbackModal() {
-  const feedbackModal = document.getElementById('feedback-modal');
-  feedbackModal.style.display = 'block';
-}
-
-function closeFeedbackModal() {
-  const feedbackModal = document.getElementById('feedback-modal');
-  feedbackModal.style.display = 'none';
-}
-
-function submitFeedback() {
-  const feedbackText = document.getElementById('feedback-text').value;
-  // You can handle the feedback submission here (e.g., send it to a server)
-  console.log('Feedback:', feedbackText);
-  closeFeedbackModal();
-}
-
-// Event Listeners
-const moodSelect = document.getElementById('mood-select');
-const colorPicker = document.getElementById('color-picker');
-const feedbackBtn = document.getElementById('feedback-btn');
-const closeBtn = document.getElementsByClassName('close')[0];
-const submitFeedbackBtn = document.getElementById('submit-feedback-btn');
-
-moodSelect.addEventListener('change', function () {
-  const selectedMood = this.value;
-  const agentRecommendation = generateAgentRecommendation(selectedMood);
-  displayAgentRecommendation(agentRecommendation);
-});
-
-colorPicker.addEventListener('change', function () {
-  const selectedColor = this.value;
-  updateBackgroundColor(selectedColor);
-});
-
-feedbackBtn.addEventListener('click', function () {
-  openFeedbackModal();
-});
-
-closeBtn.addEventListener('click', function () {
-  closeFeedbackModal();
-});
-
-submitFeedbackBtn.addEventListener('click', function () {
-  submitFeedback();
-});
+ 
